b68f54bb0c8c981129c0ffff4cfb65952dfc1e9c
JimCarretta <Jim.Carretta@noaa.gov>
2022-06-09 17:53:05 -0700

    updates

---
+++ R/Removals.R
@@ -0,0 +1,8 @@
+# Store published estimates of human-related removals from literature
+
+Species <- c("Harbor Porpoise", "Common Dolphin (all spp.)", "Humpback Whale")
+
+Year <- seq(1990, 2017, 1)
+
+removalsPP <- c(62)
+  CV.removalsPP <- c(0.44)
